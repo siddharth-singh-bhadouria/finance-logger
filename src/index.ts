dc8684f0731b2b2ebc8a18abf8c916fe1edf4573
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-27 16:10:43 +0530

    Intro to DOM

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+// form
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
+// inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
