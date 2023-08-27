0f52d237f364087d33f2be52bea507d5d6c081b3
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-27 18:34:51 +0530

    Finally completed!!

--- public/index.js
+++ public/index.js
@@ -1,3 +1,6 @@
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payment.js";
 // const invOne = new Invoice("naruto", "work on the naruto website", 10000);
 // const invTwo = new Invoice("sasuke", "work on the sasuke website", 12000);
 // let invoices: Invoice[] = [];
@@ -13,8 +16,21 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+//unordered list container
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, "end");
+    tofrom.value = "";
+    details.value = "";
+    amount.value = "";
+    console.log(doc.format());
 });
-export {};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/hasFormatter.js";
 
@@ -22,6 +23,10 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+//unordered list container
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
@@ -32,5 +37,11 @@ form.addEventListener("submit", (e: Event) => {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  list.render(doc, type.value, "end");
+
+  tofrom.value = "";
+  details.value = "";
+  amount.value = "";
+
+  console.log(doc.format());
 });
